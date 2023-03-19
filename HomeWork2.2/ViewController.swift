094ad16c6a50cb987315bbca1d83af036eea95ff
Alex <zas_st@inbox.ru>
2023-03-19 22:26:37 +0300

    first commit

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var trafficLights: UIStackView!
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    
+    @IBOutlet weak var startButtom: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButtom.layer.cornerRadius = 10
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 60
+        greenLight.layer.cornerRadius = 40
         
     }
     
-    @IBAction func startButtomDidTapped(_ sender: Any) {
+    @IBAction func startButtomDidTapped() {
+        startButtom.setTitle("NEXT", for: .normal)
     }
 }
 
