55299931bd33acc43fa42cf90e51210aef2c44b1
Alex <zas_st@inbox.ru>
2023-03-19 21:29:47 +0300

    Setup UI

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trafficLights: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func startButtomDidTapped(_ sender: Any) {
+    }
 }
 
